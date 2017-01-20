7b80852a2901c91e3f454c0dc3c984b99142c856
= <=>
2017-01-20 11:26:43 +0100

    Toni - añadiendo rutas

--- src/AppBundle/Controller/FaseunoController.php
+++ src/AppBundle/Controller/FaseunoController.php
@@ -29,4 +29,34 @@ class FaseunoController extends Controller
 
         return $this->render('AppBundle:Default:witness.html.twig', array('form' => $form->createView()));
     }
+
+    public function testigosAction()
+    {
+      return $this->render('AppBundle:Faseuno:testigos.html.twig');
+    }
+
+    public function principalAction()
+    {
+      return $this->render('AppBundle:Faseuno:principal.html.twig');
+    }
+
+    public function representanteAction()
+    {
+      return $this->render('AppBundle:Faseuno:representante.html.twig');
+    }
+
+    public function instancia_regAction()
+    {
+      return $this->render('AppBundle:Faseuno:instancia_registro.html.twig');
+    }
+
+    public function datos_persAction()
+    {
+      return $this->render('AppBundle:Faseuno:datos_personales.html.twig');
+    }
+
+    public function areasAction()
+    {
+      return $this->render('AppBundle:Faseuno:areas.html.twig');
+    }
 }
