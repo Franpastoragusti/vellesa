b93ce5c1e0aa923b8105a2fd4fc28a25c2af4d8d
Franpastoragusti <franciscopa91@gmail.com>
2017-01-21 14:45:53 +0100

    FRAN - steps animados y vinculados

---
+++ web/js/classes.js
@@ -0,0 +1,13 @@
+
+class Steps {
+  constructor(number) {
+    this.number = number;
+  }
+  stepAnimation(){
+    $("#wizard_verticle > ul > li:nth-child(1) > a").removeClass("selected").addClass("done");
+    for (var i = 0; i < this.number; i++) {
+      $("#wizard_verticle > ul > li:nth-child("+i+") > a").removeClass("disabled").addClass("done");
+    }
+    $("#wizard_verticle > ul > li:nth-child("+this.number+") > a.disabled ").first().removeClass("disabled").addClass("selected  ")
+  }
+}
