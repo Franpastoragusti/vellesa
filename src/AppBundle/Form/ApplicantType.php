1f78ad3a8a36e6c3e9a8e8c28205f0bcf12ce1a5
Franpastoragusti <franciscopa91@gmail.com>
2017-01-29 00:28:57 +0100

    FRAN - incorporada provincia en select

--- src/AppBundle/Form/ApplicantType.php
+++ src/AppBundle/Form/ApplicantType.php
@@ -8,6 +8,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class ApplicantType extends AbstractType
 {
@@ -21,11 +22,67 @@ class ApplicantType extends AbstractType
                  ->add('address', TextType::class, array('label'=>'Dirección'))
                  ->add('cp', IntegerType::class, array('label' => 'Código Postal','attr' => array('max' => 99999)))
                  ->add('city', TextType::class, array('label'=>'Ciudad'))
+                 ->add('province',ChoiceType::class, array('label'=>'Provincia','choices'  => array(
+                  'Álava' => 'Álava',
+                  'Albacete' => 'Albacete',
+                  'Alicante' => 'Alicante',
+                  'Almería' => 'Almería',
+                  'Asturias' => 'Asturias',
+                  'Ávila' => 'Ávila',
+                  'Badajoz' => 'Badajoz',
+                  'Barcelona' => 'Barcelona',
+                  'Burgos' => 'Burgos',
+                  'Cáceres' => 'Cáceres',
+                  'Cádiz' => 'Cádiz',
+                  'Cantabria' => 'Cantabria',
+                  'Castellón' => 'Castellón',
+                  'Ceuta' => 'Ceuta',
+                  'Ciudad Real' => 'Ciudad Real',
+                  'Córdoba' => 'Córdoba',
+                  'Cuenca' => 'Cuenca',
+                  'Girona' => 'Girona',
+                  'Las Palmas' => 'Las Palmas',
+                  'Granada' => 'Granada',
+                  'Guadalajara' => 'Guadalajara',
+                  'Guipúzcoa' => 'Guipúzcoa',
+                  'Huelva' => 'Huelva',
+                  'Huesca' => 'Huesca',
+                  'Islas Baleares' => 'Islas Baleares',
+                  'Jaén' => 'Jaén',
+                  'A Coruña' => 'A Coruña',
+                  'La Rioja' => 'La Rioja',
+                  'León' => 'León',
+                  'Lleida' => 'Lleida',
+                  'Lugo' => 'Lugo',
+                  'Madrid' => 'Madrid',
+                  'Málaga' => 'Málaga',
+                  'Melilla' => 'Melilla',
+                  'Murcia' => 'Murcia',
+                  'Navarra' => 'Navarra',
+                  'Ourense' => 'Ourense',
+                  'Palencia' => 'Palencia',
+                  'Pontevedra' => 'Pontevedra',
+                  'Salamanca' => 'Salamanca',
+                  'Segovia' => 'Segovia',
+                  'Sevilla' => 'Sevilla',
+                  'Soria' => 'Soria',
+                  'Tarragona' => 'Tarragona',
+                  'Santa Cruz de Tenerife' => 'Santa Cruz de Tenerife',
+                  'Teruel' => 'Teruel',
+                  'Toledo' => 'Toledo',
+                  'Valencia' => 'Valencia',
+                  'Valladolid' => 'Valladolid',
+                  'Vizcaya' => 'Vizcaya',
+                  'Zamora' => 'Zamora',
+                  'Zaragoza' => 'Zaragoza'
+                      )
+                    ))
                  ->add('phone', IntegerType::class, array('label' => 'Teléfono','attr' => array('max' => 999999999)))
                  ->add('urldnifront', TextType::class, array('label'=>'DNI Delante'))
                  ->add('urldnibehind', TextType::class, array('label'=>'DNI Detrás'))
-                 ->add('province', TextType::class, array('label'=>'provincia'))
                  ->add('save', SubmitType::class, array('label' => 'Siguiente','attr' => array('class' => 'btn btn-warning start')));
+
+
      }
 
     /**
--- src/AppBundle/Form/WitnessType.php
+++ src/AppBundle/Form/WitnessType.php
@@ -8,6 +8,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 
 class WitnessType extends AbstractType
@@ -22,7 +23,61 @@ class WitnessType extends AbstractType
             ->add('addres', TextType::class, array('label'=>'Dirección'))
             ->add('cp', IntegerType::class, array('label' => 'Código Postal','attr' => array('max' => 99999)))
             ->add('city', TextType::class, array('label'=>'Ciudad'))
-            ->add('province', TextType::class, array('label'=>'Provincia'))
+            ->add('province',ChoiceType::class, array('label'=>'Provincia','choices'  => array(
+             'Álava' => 'Álava',
+             'Albacete' => 'Albacete',
+             'Alicante' => 'Alicante',
+             'Almería' => 'Almería',
+             'Asturias' => 'Asturias',
+             'Ávila' => 'Ávila',
+             'Badajoz' => 'Badajoz',
+             'Barcelona' => 'Barcelona',
+             'Burgos' => 'Burgos',
+             'Cáceres' => 'Cáceres',
+             'Cádiz' => 'Cádiz',
+             'Cantabria' => 'Cantabria',
+             'Castellón' => 'Castellón',
+             'Ceuta' => 'Ceuta',
+             'Ciudad Real' => 'Ciudad Real',
+             'Córdoba' => 'Córdoba',
+             'Cuenca' => 'Cuenca',
+             'Girona' => 'Girona',
+             'Las Palmas' => 'Las Palmas',
+             'Granada' => 'Granada',
+             'Guadalajara' => 'Guadalajara',
+             'Guipúzcoa' => 'Guipúzcoa',
+             'Huelva' => 'Huelva',
+             'Huesca' => 'Huesca',
+             'Islas Baleares' => 'Islas Baleares',
+             'Jaén' => 'Jaén',
+             'A Coruña' => 'A Coruña',
+             'La Rioja' => 'La Rioja',
+             'León' => 'León',
+             'Lleida' => 'Lleida',
+             'Lugo' => 'Lugo',
+             'Madrid' => 'Madrid',
+             'Málaga' => 'Málaga',
+             'Melilla' => 'Melilla',
+             'Murcia' => 'Murcia',
+             'Navarra' => 'Navarra',
+             'Ourense' => 'Ourense',
+             'Palencia' => 'Palencia',
+             'Pontevedra' => 'Pontevedra',
+             'Salamanca' => 'Salamanca',
+             'Segovia' => 'Segovia',
+             'Sevilla' => 'Sevilla',
+             'Soria' => 'Soria',
+             'Tarragona' => 'Tarragona',
+             'Santa Cruz de Tenerife' => 'Santa Cruz de Tenerife',
+             'Teruel' => 'Teruel',
+             'Toledo' => 'Toledo',
+             'Valencia' => 'Valencia',
+             'Valladolid' => 'Valladolid',
+             'Vizcaya' => 'Vizcaya',
+             'Zamora' => 'Zamora',
+             'Zaragoza' => 'Zaragoza'
+                 )
+               ))
             ->add('number')
             ->add('representant')
             ->add('phone', IntegerType::class, array('label' => 'Teléfono','attr' => array('max' => 999999999)))
