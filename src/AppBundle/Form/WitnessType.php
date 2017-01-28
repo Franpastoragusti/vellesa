6376fc8228e99267ea702f355608c06a2323c9c1
Franpastoragusti <franciscopa91@gmail.com>
2017-01-28 22:54:57 +0100

    FRAN - Ajustes en Witness Type

--- src/AppBundle/Form/WitnessType.php
+++ src/AppBundle/Form/WitnessType.php
@@ -3,9 +3,12 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class WitnessType extends AbstractType
 {
@@ -14,16 +17,16 @@ class WitnessType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name')
-            ->add('surname')
-            ->add('addres')
-            ->add('cp')
-            ->add('city')
+        $builder->add('name', TextType::class, array('label'=>'Nombre'))
+            ->add('surname', TextType::class, array('label'=>'Apellidos'))
+            ->add('addres', TextType::class, array('label'=>'Dirección'))
+            ->add('cp', IntegerType::class, array('label' => 'Código Postal','attr' => array('max' => 99999)))
+            ->add('city', TextType::class, array('label'=>'Ciudad'))
             ->add('number')
             ->add('representant')
-            ->add('phone')
-            ->add('urldnifront')
-            ->add('urldnibehind')
+            ->add('phone', IntegerType::class, array('label' => 'Teléfono','attr' => array('max' => 999999999)))
+            ->add('urldnifront', TextType::class, array('label'=>'DNI Delante'))
+            ->add('urldnibehind', TextType::class, array('label'=>'DNI Detrás'))
             ->add('users')
             ->add('save', SubmitType::class, array('label' => 'Siguiente'))
         ;
