8acd3cc13bdc5c44fd5609ac377fe77323a64231
Franpastoragusti <franciscopa91@gmail.com>
2017-02-13 10:45:22 +0100

    FRAN - retoques family area a medias

--- src/AppBundle/Form/FamilyAreaType.php
+++ src/AppBundle/Form/FamilyAreaType.php
@@ -3,15 +3,14 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
@@ -58,7 +57,7 @@ class FamilyAreaType extends AbstractType
                 'expanded' => true,
                 'multiple'  => false,
             ))
-            
+
             ->add('visits', ChoiceType::class, array(
                 'expanded' => true,
                 'multiple' => true,
@@ -88,7 +87,7 @@ class FamilyAreaType extends AbstractType
         ;
 
     }
-    
+
     /**
      * {@inheritdoc}
      */
