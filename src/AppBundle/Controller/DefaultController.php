b44acce6bbfc791431550f3b2513898cae2198d1
tonymateo <tonymateo95@gmail.com>
2017-02-17 19:54:05 +0100

    TONI - Pdf construido (falta area familia)

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,21 +18,36 @@ class DefaultController extends Controller
 
     public function pdfAction()
     {
-      $personal_data = $this->getDoctrine()->getRepository('AppBundle:PersonalData')->findBypersonclassId(1);
-      $witness_data = $this->getDoctrine()->getRepository('AppBundle:PersonalData')->findBypersonclassId(2);
-      $representant_data = $this->getDoctrine()->getRepository('AppBundle:PersonalData')->findBypersonclassId(3);
 
-      //$name = $personal_data->getName();
-      // $surname = $personal_data->getSurname();
-      // $sip = $personal_data->getSip();
-      // $phone = $personal_data->getPhone();
-      // $dni = $personal_data->getDni();
+      $user = $this->getUser()->getId();
+      $em = $this->getDoctrine()->getManager();
+      $query = $em->createQuery(
+          'SELECT p AS personalData, d AS direction
+          FROM AppBundle:PersonalData p, AppBundle:Direction d
+          WHERE p.userId = :userId
+          AND d.id = p.direction'
+      )->setParameter('userId' , $user);
 
+      $data=$query->getResult();
+      $directionAplicant = $data[0]['personalData']->getDirection();
+      $directionWitness1 = $data[2]['personalData']->getDirection();
+      $directionWitness2 = $data[4]['personalData']->getDirection();
+      $directionWitness3 = $data[6]['personalData']->getDirection();
+      $directionRepresentant = $data[8]['personalData']->getDirection();
+
+      $healthData = $this->getDoctrine()->getRepository('AppBundle:HealthArea')->findOneByuserId($user);
+      $environmentData = $this->getDoctrine()->getRepository('AppBundle:EnvironmentArea')->findOneByuserId($user);
+      $personalAreaData = $this->getDoctrine()->getRepository('AppBundle:PersonalArea')->findOneByuserId($user);
+      $familyData = $this->getDoctrine()->getRepository('AppBundle:FamilyArea')->findOneByuserId($user);
 
         return $this->render('AppBundle:Default:pdf.html.twig', array(
-          "personal_data" => $personal_data,
-          "witness_data" => $witness_data,
-          "representant_data" => $representant_data
+          "applicant" => $data[0]['personalData'], 'directionAplicant' => $directionAplicant,
+          "witness1" => $data[2]['personalData'], 'directionWitness1' => $directionWitness1,
+          "witness2" => $data[4]['personalData'], 'directionWitness2' => $directionWitness2,
+          "witness3" => $data[6]['personalData'], 'directionWitness3' => $directionWitness3,
+          "representant" => $data[8]['personalData'], 'directionRepresentant' => $directionRepresentant,
+          "healthData" => $healthData, 'environmentData' => $environmentData,
+          "personalAreaData" => $personalAreaData, 'familyData' => $familyData
         ));
     }
 
--- web/js/class.assistant.js
+++ web/js/class.assistant.js
@@ -9,9 +9,38 @@ class Assistant {
         this.textoApp = $("#texto_app");
         this.textoUser = $("#texto_user");
         this.popupUser = $("#mensaje_user").css("display", "none");
-        //this.text = "";
         this.id = 1;
+        // this.texts = {
+        //     default:"Hola estas en this.route",
+        //     timeMessages:{
+        //       first:{
+        //         time:5,
+        //         message:"Han pasado 5 minutos"
+        //       },
+        //       second:{
+        //         time:10,
+        //         message:"Han pasado 10 minutos"
+        //       },
+        //       last:{
+        //         time:15,
+        //         message:"Han pasado 15 minutos"
+        //       }
+        //     },
+        //     areaText:{
+        //       family:"defect text"
+        //       health:"defecto salus"
+        //       personal:"defecto personal"
+        //       enviroment:""
+        //     }
+        // }
     }
+
+    getRoute(){
+      var posicion = window.location.href.indexOf("/app");
+      var pantalla = window.location.href.substring(posicion);
+      return pantalla;
+    }
+
 
     showAssistant() {
         $(function() {
@@ -59,57 +88,6 @@ class Assistant {
         });
     }
 
-    customAnswer() {
-        switch (this.pantalla) {
-            case "/app/testRoom":
-                //Escribir el texto de nuestro "bot"
-                this.text = "<p>Hola estas en chatRoom</p>";
-                break;
-            case "/app/areas/health":
-                //Escribir el texto de nuestro "bot"
-                this.text = "<p>Hola estoy en health</p>";
-                break;
-            case "/app/areas/personal":
-                //Escribir el texto de nuestro "bot"
-                this.text = "<p>Hola estoy en personal</p>";
-                break;
-            case "/app/areas/family":
-                //Escribir el texto de nuestro "bot"
-                this.text = ("<p>Hola estoy en familia</p>");
-                break;
-            case "/app/bureaucracy":
-                //Escribir el texto de nuestro "bot"
-                this.text = ("<p>Hola estoy en datos</p>");
-                break;
-            case "/app/bureaucracy/0":
-                //Escribir el texto de nuestro "bot"
-                this.text = ("<p>Hola estoy en tus datos personales</p>");
-                break;
-            case "/app/bureaucracy/1":
-                //Escribir el texto de nuestro "bot"
-                this.text = ("<p>Hola estoy en testigo 1</p>");
-                break;
-            case "/app/bureaucracy/2":
-                //Escribir el texto de nuestro "bot"
-                this.text = ("<p>Hola estoy en testigo 2</p>");
-                break;
-            case "/app/bureaucracy/3":
-                //Escribir el texto de nuestro "bot"
-                this.text = ("<p>Hola estoy en testigo 3</p>");
-                break;
-            case "/app/bureaucracy/4":
-                //Escribir el texto de nuestro "bot"
-                this.text = ("<p>Hola estoy en representante</p>");
-                break;
-            case "/app/bureaucracy/instance":
-                //Escribir el texto de nuestro "bot"
-                this.text = ("<p>Hola estoy en instance</p>");
-                break;
-            default:
-        }
-
-
-    }
 
     appAnswer(text){
       this.textoApp.append(text);
