02c6d5f7a984259aefaa8c1972e5e5a36e632099
tonymateo <tonymateo95@gmail.com>
2017-02-22 18:49:11 +0100

    TONI - arreglado orden imagenes pdf + autoloader

--- web/js/getImageAsync.js
+++ web/js/getImageAsync.js
@@ -1,20 +1,18 @@
-function getImageAsync(rutas, base64) {
-  for (var i = 0; i < rutas.length; i++) {
+function getImageAsync(imagen, base64) {
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", rutas[i], true);
+        xhr.open("GET", imagen, true);
         xhr.responseType = "blob";
         xhr.onload = function (e) {
                 var reader = new FileReader();
                 reader.onload = function(event) {
                   var res = event.target.result;
-                  if (base64.length != rutas.length) {
+                  if (base64.length != 1) {
                     console.log("Generando imagenes..");
                   }
-                  base64.push(res);
+                  base64.push(res)
                 }
                 var file = this.response;
                 reader.readAsDataURL(file)
         };
         xhr.send()
-    }
 }
